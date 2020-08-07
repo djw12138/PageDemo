0e8e7adabb704b93bf43bbdc330b9f5118f1ffab
DESKTOP-D18M4CI\ZTT <zss93157623@qq.com>
2020-08-07 19:22:05 +0800

    地图

--- src/main/java/com/page/controller/EchartsController.java
+++ src/main/java/com/page/controller/EchartsController.java
@@ -14,7 +14,7 @@ public class EchartsController {
     private static String PREFIX = "pages/echarts";
 
     /**
-     * 测试一
+     * 测试一(柱状图)
      * @return
      */
     @RequestMapping(value = "/demo1" , method = RequestMethod.GET)
@@ -23,7 +23,7 @@ public class EchartsController {
     }
 
     /**
-     * 测试二
+     * 测试二(饼图)
      * @return
      */
     @RequestMapping(value = "/demo2" , method = RequestMethod.GET)
@@ -32,11 +32,29 @@ public class EchartsController {
     }
 
     /**
-     * 测试三
+     * 测试三(地图)
      * @return
      */
     @RequestMapping(value = "/demo3" , method = RequestMethod.GET)
     public String demo3() {
         return PREFIX+"/demo3";
+    }
+
+    /**
+     * 测试四(地图)
+     * @return
+     */
+    @RequestMapping(value = "/demo4" , method = RequestMethod.GET)
+    public String demo4() {
+        return PREFIX+"/demo4";
+    }
+
+    /**
+     * 测试五(主题)
+     * @return
+     */
+    @RequestMapping(value = "/demo5" , method = RequestMethod.GET)
+    public String demo5() {
+        return PREFIX+"/demo5";
     }
 }
